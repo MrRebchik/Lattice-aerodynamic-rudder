6d6af198528ca89c9668b12a3d122734de195d80
Penulltimo <38166546+MrRebchik@users.noreply.github.com>
2024-10-02 13:26:00 +0300

    Создание проекта и шаблонов классов

---
+++ LatticeAerodynamicRudder/AerounamicRudder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AerodynamicRudder
+{
+    public abstract class AerounamicRudder
+    {
+        public abstract decimal GetAreaWithCurrentAngel(decimal angle);
+    }
+}
---
+++ LatticeAerodynamicRudder/LatticeAeroRudder.cs
@@ -0,0 +1,17 @@
+﻿using AerodynamicRudder;
+
+namespace LatticeAerodynamicRudder
+{
+    public class LatticeAeroRudder : AerounamicRudder
+    {
+        public decimal Height { get; set; }
+        public decimal Width { get; set; }
+        public decimal Depth { get; set; }
+        public decimal BorderThickness { get; set; }
+        public decimal InnerEdgeThickness { get; set; }
+        public override decimal GetAreaWithCurrentAngel(decimal angle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
